9bdbe924a959c36d404caab83651a1b21d1b8dbd
Gorgeous BSC token <gorgeous.token@gmail.com>
2021-06-24 16:39:19 +0000

    Fix netlify deploy

--- src/components/Header/Menu/Share.tsx
+++ src/components/Header/Menu/Share.tsx
@@ -59,18 +59,8 @@ const Share = () => {
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         style={customStyles}
-        contentLabel="Example Modal"
       >
-        <h2>Hello</h2>
-        <button onClick={closeModal}>close</button>
-        <div>I am a modal</div>
-        <form>
-          <input />
-          <button>tab navigation</button>
-          <button>stays</button>
-          <button>inside</button>
-          <button>the modal</button>
-        </form>
+        Share
       </Modal>
     </>
   );
