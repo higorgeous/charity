67552fcc57ce24f2f9a28d2230f9d4cc0fdf1a23
Gorgeous BSC token <gorgeous.token@gmail.com>
2021-06-26 14:43:23 +0000

    Test env

--- src/services/Firebase/Client.ts
+++ src/services/Firebase/Client.ts
@@ -7,14 +7,22 @@ import 'firebase/performance';
 
 import isClientSide from '@utils/isClientSide';
 
+const apiKey = process.env.FIREBASE_API_KEY;
+const authDomain = process.env.FIREBASE_AUTH_DOMAIN;
+const projectId = process.env.FIREBASE_PROJECT_ID;
+const storageBucket = process.env.FIREBASE_STORAGE_BUCKET;
+const messagingSenderId = process.env.FIREBASE_MESSAGING_SENDER_ID;
+const appId = process.env.FIREBASE_APP_ID;
+const measurementId = process.env.FIREBASE_MEASUREMENT_ID;
+
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
 };
 
 if (!firebaseClient.apps.length) {
