7898dda9e8bf5612263d5c87b0975f5971118fcf
Gorgeous BSC token <gorgeous.token@gmail.com>
2021-06-26 14:15:42 +0000

    Fix env

--- src/services/Firebase/Admin.ts
+++ src/services/Firebase/Admin.ts
@@ -1,8 +1,8 @@
 import * as firebaseAdmin from 'firebase-admin';
 
-const privateKey = process.env['FIREBASE_PRIVATE_KEY'];
-const clientEmail = process.env['FIREBASE_CLIENT_EMAIL'];
-const projectId = process.env['FIREBASE_PROJECT_ID'];
+const privateKey = process.env.FIREBASE_PRIVATE_KEY;
+const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+const projectId = process.env.FIREBASE_PROJECT_ID;
 
 if (!privateKey || !clientEmail || !projectId) {
   console.log(
