33768d5df90540e659fe011c9da78346f3a3b337
Gorgeous BSC token <gorgeous.token@gmail.com>
2021-06-29 02:33:17 +0000

    Update account

--- src/screens/Account/index.tsx
+++ src/screens/Account/index.tsx
@@ -4,7 +4,7 @@ import { Wrapper } from './styles';
 
 const AccountScreen = () => (
   <Wrapper>
-    <Hero>Account</Hero>
+    <Hero>Your account</Hero>
     <p className="text">View your voting history and charity submissions.</p>
   </Wrapper>
 );
