3ff31431d86a91c50041a4e255343f2d017a93a0
Gorgeous BSC token <gorgeous.token@gmail.com>
2021-07-05 23:06:55 +0000

    Add segment

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,34 +1,20 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import * as snippet from '@segment/snippet';
-
-const {
-  // This write key is associated with https://segment.com/nextjs-example/sources/nextjs.
-  ANALYTICS_WRITE_KEY = process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY,
-  NODE_ENV = 'development',
-} = process.env;
 
 export default class MyDocument extends Document {
-  renderSnippet() {
-    const opts = {
-      apiKey: ANALYTICS_WRITE_KEY,
-      // note: the page option only covers SSR tracking.
-      // Page.js is used to track other events using `window.analytics.page()`
-      page: true,
-    };
-
-    if (NODE_ENV === 'development') {
-      return snippet.max(opts);
-    }
-
-    return snippet.min(opts);
-  }
-
   render() {
     return (
       <Html>
         <Head>
-          {/* Inject the Segment snippet into the <head> of the document  */}
-          <script dangerouslySetInnerHTML={{ __html: this.renderSnippet() }} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+        !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
+            analytics.load("${process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY}");
+            analytics.page();
+        }}();
+    `,
+            }}
+          ></script>
         </Head>
         <body>
           <Main />
