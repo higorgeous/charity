e195d507360ae9bf23bc47f2f3d29a1b2b621a3a
Gorgeous BSC token <gorgeous.token@gmail.com>
2021-07-05 14:03:41 +0000

    Fix about styling

--- src/screens/About/styles.ts
+++ src/screens/About/styles.ts
@@ -4,12 +4,12 @@ export const Wrapper = styled.div`
   width: 100%;
   text-align: center;
   margin-bottom: 70px;
-  p {
+  .sentence {
     max-width: 700px;
     font-size: 15px;
     font-weight: 700;
     text-align: center;
-    margin: 50px auto 100px;
+    margin: 20px auto 30px;
     @media (max-width: 767px) {
       margin: 30px 22px;
       font-size: 14px;
