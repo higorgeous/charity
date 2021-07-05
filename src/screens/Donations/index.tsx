190a4df8a99db52e916c2846f86bbb0bb43f91ac
Gorgeous BSC token <gorgeous.token@gmail.com>
2021-07-05 01:49:03 +0000

    Update donations page

--- src/screens/Donations/index.tsx
+++ src/screens/Donations/index.tsx
@@ -9,8 +9,8 @@ const DonationsScreen = () => (
     <Hero>Donations made by the Gorgeous community</Hero>
     <p className="text">
       Our first donation will be made to the charity, organisation or cause with
-      the most votes on Monday 5th July 2021. Details and receipts will be shown on
-      this page.
+      the most votes on Friday 9th July 2021. Details and receipts will be shown
+      on this page.
     </p>
     <EmptyState />
   </Wrapper>
