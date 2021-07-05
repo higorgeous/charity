e7a96afed966ad35af3588ce5f723ee02f978f6b
Gorgeous BSC token <gorgeous.token@gmail.com>
2021-07-05 08:25:40 +0000

    Add edit links in account submission history

--- src/components/HistorySubmission/index.tsx
+++ src/components/HistorySubmission/index.tsx
@@ -1,6 +1,6 @@
 import { useMemo, FC } from 'react';
 
-import Profile from '../TableElements/Profile';
+import ProfileHistory from '../TableElements/ProfileHistory';
 import CreatedAt from '../TableElements/CreatedAt';
 import TableData from '../TableElements/TableData';
 
@@ -9,7 +9,7 @@ import { Wrapper } from '../TableElements/styles';
 const HistorySubmissions: FC<any> = ({ submissionData }) => {
   const data = submissionData.map((charity: any, index: number) => {
     return {
-      charity: Profile(charity, index),
+      charity: ProfileHistory(charity, index),
       createdAt: CreatedAt(charity),
     };
   });
---
+++ src/components/TableElements/ProfileHistory.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { CharityColumn } from './styles';
+
+const ProfileHistory = (charity: any, index: number) => (
+  <CharityColumn>
+    <Link href={`/charity/edit/${charity.id}`}>
+      <a>
+        <span className="rank">{index + 1}</span>
+        <span className="logo">
+          <Image src={charity.logo} alt={charity.name} width="50" height="50" />
+        </span>
+        <span className="profile">
+          <span className="name">{charity.name} ⓘ</span>
+          <span className="tag">{charity.tag}</span>
+        </span>
+      </a>
+    </Link>
+  </CharityColumn>
+);
+
+export default ProfileHistory;
